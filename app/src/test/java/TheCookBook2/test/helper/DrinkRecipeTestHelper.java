40e8d31f5e2546671561ab02f174115bb5140a50
Chase Fowler <chasefowler51@gmail.com>
2023-07-03 18:22:35 -0500

    added DrinkRecipeTestHelper

---
+++ app/src/test/java/TheCookBook2/test/helper/DrinkRecipeTestHelper.java
@@ -0,0 +1,34 @@
+package TheCookBook2.test.helper;
+
+import TheCookBook2.dynamodb.models.DrinkRecipe;
+
+import java.util.*;
+
+public class DrinkRecipeTestHelper {
+    public static DrinkRecipe generateDrinkRecipe() {
+        DrinkRecipe drinkRecipe = new DrinkRecipe();
+        drinkRecipe.setCreator("creator");
+        drinkRecipe.setRecipeTitle("recipe title");
+        Map<String, String> ingredients = new HashMap<>();
+        ingredients.put("ingredient", "amount");
+        drinkRecipe.setIngredients(ingredients);
+        LinkedList<String> instructionSteps = new LinkedList<>();
+        instructionSteps.add("step 1");
+        drinkRecipe.setInstructionSteps(instructionSteps);
+        drinkRecipe.setDescription("description");
+        Set<String> descriptionTags = new HashSet<>();
+        descriptionTags.add("description tag");
+        drinkRecipe.setDescriptionTags(descriptionTags);
+        drinkRecipe.setDrinkCategory("drink category");
+        drinkRecipe.setDrinkItem("drink item");
+        Set<String> allergies = new HashSet<>();
+        drinkRecipe.setAllergies(allergies);
+        Map<Integer, Integer> ratings = new HashMap<>();
+        ratings.put(-1, 0);
+        ratings.put(0, 0);
+        ratings.put(1, 0);
+        drinkRecipe.setRatings(ratings);
+
+        return drinkRecipe;
+    }
+}
